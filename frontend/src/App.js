d7c906ee8a803bc947a407ab38f8a4f22a8f60fd
valeria <valeriagrandi@hotmail.com>
2020-04-11 20:05:31 -0300

    barra de navegacion,crea un cliente

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import { BrowserRouter as Router,Switch, Path } from "react-router-dom";
-import Menu from "./components/Menu.js"
-import "./App.css";
+import React from "react"; 
+import { BrowserRouter as Router,Route, Switch, Path, Link } from "react-router-dom"; 
+import FormularioCliente from "./components/FormularioCliente"; 
+import "./App.css"; 
 
 
-function App() {
+function App() { 
   return (
-      <div className="contenedor">
+    <div className="contenedor">
     <Router>
         <header>
-        
-        <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo right">Ellas y Nosotras</a>
-                <Menu></Menu>
-               
-                
-               
-            </div>
-            {/* <FormularioArticulo></FormularioArticulo> */}
-           
-            
-        </nav>
-        
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo right">
+                      Ellas y Nosotras
+                    </a>
+                    <ul id="menuCliente" class="dropdown-content">
+                        <li><Link to="/agregarCliente">Agregar cliente</Link></li>
+                        <li><Link to="/">Cliente Cuenta Corriente</Link></li>
+                        <li class="divider"></li>
+                        <li><Link to="/">Listado de clientes</Link></li>
+                    </ul>
+                    <ul id="nav-mobile" class="left hide-on-med-and-down">
+                        <li><Link to="/">Inicio</Link></li>
+                        <li><Link to="/">Artículo</Link></li>
+                        <li><a class="dropdown-trigger" href="#!" data-target="menuCliente">Cliente
+                            <i class="material-icons right">arrow_drop_down</i></a></li>
+                        <li><Link to="/">Facturación</Link></li>
+                    </ul>
+                </div>
+            </nav>
         </header>
         <main>
-        <Switch>
-        {/* <Route path="/articulos" component={ArticulosComponent} /> */}
-        </Switch>
+            <Switch>
+                <Route path="/agregarCliente" component={FormularioCliente} />
+            </Switch>
         </main>
-      </Router>
-      </div>
-    );
-}
-
+    </Router>
+</div>
+  );
+ } 
+ 
 export default App;
