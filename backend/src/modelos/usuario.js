687dff4f5d985206d392dda1dfea94b361884fe9
valeria <valeriagrandi@hotmail.com>
2020-06-25 09:03:00 -0300

    agregado del atributo dni al modelo de usuario

--- backend/src/modelos/usuario.js
+++ backend/src/modelos/usuario.js
@@ -1,9 +1,10 @@
 class Usuario {
-    constructor(_email, _password,_usuario){
+    constructor(_email, _password,_usuario, _dni){
   
     this.username= _email;
     this.password=_password;
     this.rol= _usuario;
+    this.dni=_dni
   }
 }
 module.exports= Usuario;
